b13a060fce8ce19f037c5f9bc998411fb3614707
Ayush <ayush1314.smn@gmail.com>
2026-07-02 22:41:58 +0530

    Setup Express backend and MongoDB connection

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.use(cookieParser());
+
+app.use(helmet());
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "AI Ticket System API Running"
+    });
+});
+
+module.exports = app;
